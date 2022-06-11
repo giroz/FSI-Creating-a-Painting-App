6f33507ef9df2878f17b3f42094ae541cd963c9d
Matthew Toledo <matthewftoledo@gmail.com>
2022-06-10 23:11:14 -0700

    last javascript

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+let selectedColor = 'blue'
 const painting = document.querySelector('.painting')
 
 painting.addEventListener('click', function(e){
-	e.target.style.backgroundColor = 'blue'
+	e.target.style.backgroundColor = selectedColor 
 })
